38fedf32adaf42b3f570fbd252d5bfa3047bacae
Huss3n <aisackhussein@gmail.com>
2023-12-25 17:04:47 +0300

    Initial commit

---
+++ Chatty/ChattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattyApp.swift
+//  Chatty
+//
+//  Created by Muktar Hussein on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
