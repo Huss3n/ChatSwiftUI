b90e60a9ad0da31870082503f142554c85d4c77a
Huss3n <aisackhussein@gmail.com>
2023-12-27 22:50:44 +0300

    Auth refactor

---
+++ Chatty/Auth/ViewModels/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewModel.swift
+//  Chatty
+//
+//  Created by Muktar Hussein on 27/12/2023.
+//
+
+import Foundation
+
+
+@MainActor
+final class LoginViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    
+    
+    
+}
---
+++ Chatty/Auth/ViewModels/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  Chatty
+//
+//  Created by Muktar Hussein on 27/12/2023.
+//
+
+import Foundation
