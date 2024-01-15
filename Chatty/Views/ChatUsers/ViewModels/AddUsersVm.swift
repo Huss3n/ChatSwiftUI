11a6b67bc51fe7544d8168874a216ecd5f212464
Huss3n <aisackhussein@gmail.com>
2024-01-15 23:34:50 +0300

    Made changes to app background color. Completed 0auth functionalities.

---
+++ Chatty/Views/ChatUsers/ViewModels/AddUsersVm.swift
@@ -0,0 +1,8 @@
+//
+//  AddUsersVm.swift
+//  Chatty
+//
+//  Created by Muktar Hussein on 08/01/2024.
+//
+
+import Foundation
---
+++ Chatty/Views/ChatUsers/Views/AddChatUsers.swift
@@ -0,0 +1,18 @@
+//
+//  AddChatUsers.swift
+//  Chatty
+//
+//  Created by Muktar Hussein on 08/01/2024.
+//
+
+import SwiftUI
+
+struct AddChatUsers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddChatUsers()
+}
